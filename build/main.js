aca10c68b12abbe24302f7bee0b94d3f45358ef7
Simon Opheim <Simon@Simons-MacBook-Air.local>
2019-08-14 19:42:00 +0200

    added support for touchscreen

--- build/main.js
+++ build/main.js
@@ -14,8 +14,17 @@ restart(canvas, TOTAL_BEES);
 restartButton.addEventListener('click', function () { return restart(canvas, TOTAL_BEES); });
 canvas.oncontextmenu = function () { return false; };
 canvas.addEventListener('mousedown', handleClick);
+var touchStart = 0;
+var touchEnd = 0;
+canvas.addEventListener('touchend', handleTouch);
+canvas.addEventListener('touchstart', function (e) {
+    e.preventDefault();
+    touchStart = Date.now();
+});
 function handleClick(event) {
     event.preventDefault();
+    if (game.gameOver)
+        return;
     var rect = this.getBoundingClientRect();
     var x = event.clientX - rect.left;
     var y = event.clientY - rect.top;
@@ -48,3 +57,37 @@ function handleClick(event) {
     }
 }
 ;
+function handleTouch(event) {
+    event.preventDefault();
+    touchEnd = Date.now();
+    var rect = this.getBoundingClientRect();
+    var x = event.touches[0].clientX - rect.left;
+    var y = event.touches[0].clientY - rect.top;
+    if (touchEnd - touchStart > 500) {
+        for (var i = 0; i < game.cols; i++) {
+            for (var j = 0; j < game.rows; j++) {
+                if (game.grid[i][j].contains(x, y)) {
+                    game.grid[i][j].flag(this);
+                }
+            }
+        }
+    }
+    else {
+        for (var i = 0; i < game.cols; i++) {
+            for (var j = 0; j < game.rows; j++) {
+                if (game.grid[i][j].contains(x, y)) {
+                    if (game.grid[i][j].flagged) {
+                        game.grid[i][j].flagged = false;
+                    }
+                    game.grid[i][j].reveal(this, game.grid);
+                }
+            }
+        }
+    }
+    game.updateGame();
+    flaggedElement.textContent = String(game.flagged);
+    if (game.gameOver) {
+        game.showAll();
+        flaggedElement.textContent = "Du scoret " + game.score + " av " + game.totalBees + " mulige.";
+    }
+}
--- main.ts
+++ main.ts
@@ -18,8 +18,18 @@ restartButton.addEventListener('click', () => restart(canvas, TOTAL_BEES));
 canvas.oncontextmenu = () => false;
 canvas.addEventListener('mousedown', handleClick);
 
+let touchStart = 0;
+let touchEnd = 0;
+
+canvas.addEventListener('touchend', handleTouch);
+canvas.addEventListener('touchstart', (e) => {
+  e.preventDefault();
+  touchStart = Date.now();
+})
+
 function handleClick(this: HTMLCanvasElement, event: MouseEvent) {
     event.preventDefault();
+    if(game.gameOver) return;
 
     const rect = this.getBoundingClientRect();
     const x = event.clientX - rect.left;
@@ -53,3 +63,38 @@ function handleClick(this: HTMLCanvasElement, event: MouseEvent) {
       flaggedElement.textContent = `Du scoret ${game.score} av ${game.totalBees} mulige.`;
     } 
   };
+
+  function handleTouch(this: HTMLCanvasElement, event: TouchEvent) {
+    event.preventDefault();
+    touchEnd = Date.now();
+    const rect = this.getBoundingClientRect();
+    const x = event.touches[0].clientX - rect.left;
+    const y = event.touches[0].clientY - rect.top;
+    if(touchEnd - touchStart > 500) {
+      for(let i = 0; i < game.cols; i++) {
+        for(let j = 0; j < game.rows; j++) {
+          if(game.grid[i][j].contains(x,y)) {
+            game.grid[i][j].flag(this);
+          }
+        }
+      }
+    } else {
+      for(let i = 0; i < game.cols; i++) {
+        for(let j = 0; j < game.rows; j++) {
+          if(game.grid[i][j].contains(x,y)) {
+            if(game.grid[i][j].flagged){
+              game.grid[i][j].flagged = false;
+            } 
+            game.grid[i][j].reveal(this, game.grid);
+          }
+        }
+      }
+    }
+    game.updateGame();
+    flaggedElement.textContent = String(game.flagged);
+
+    if(game.gameOver) {
+      game.showAll();
+      flaggedElement.textContent = `Du scoret ${game.score} av ${game.totalBees} mulige.`;
+    } 
+  }
